4115dbd01162d7ff34091232c7e9ec5980594cfe
Tiago Cardoso <tcardoso@nuxeo.com>
2016-07-21 23:41:50 +0100

    adds glTF conversion to process

--- process-models.sh
+++ process-models.sh
@@ -14,15 +14,15 @@ do
   echo render to png: $output_dir/render.png
 
   blender -b -P scripts/convert.py -- $file_path $output_dir/conversion.gltf
-  #collada2gltf -f $output_dir/conversion.dae -e -o $output_dir/conversion.gltf
+  collada2gltf -f $output_dir/conversion.dae -e -o $output_dir/conversion.gltf
   #rm $output_dir/conversion.dae
 
-  lods=( 50 25 15 7 3)
+  lods=( 50 25 15 7 3) #percentage
   for lod in "${lods[@]}"
   do
     echo "rendering LOD of $lod"
     blender -b -P scripts/lod.py -- $file_path $output_dir/conversion-${lod}.dae $lod
     blender -b -F PNG -P scripts/render.py -- $output_dir/conversion-${lod}.dae $output_dir/render-${lod} 960 540
-    #collada2gltf -f $output_dir/conversion-${lod}.dae -o $output_dir/conversion-${lod}.gltf -e
+    collada2gltf -f $output_dir/conversion-${lod}.dae -o $output_dir/conversion-${lod}.gltf -e
   done
 done
