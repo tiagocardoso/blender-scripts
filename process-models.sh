2a0aabd95dd20252e071db6c5b037179118f56d4
Tiago Cardoso <tcardoso@nuxeo.com>
2016-09-01 11:40:08 +0100

    Change to Docker execution

--- process-models.sh
+++ process-models.sh
@@ -9,22 +9,27 @@ for file_path in models/*.???
 do
 
   file=${file_path##*/}
-  output_dir=output/${file%.*}
+  output_dir=${file%.*}
   echo processing: $file
-
   lods=( "2" "3" "1") #percentage
   operators="import render convert "
   for lod in "${lods[@]}"
   do
     operators+=" lod convert render"
   done
-  blender -b -P scripts/pipeline.py -- --input $file_path --outdir $output_dir --operators $operators --lods ${lods[*]// / } --width 960 --height 600 --coords 0,0 90,0 0,90 90,90
+  docker run --rm -v /Users/tiago/Nuxeo/projects/blender-scripts/scripts:/scripts:ro \
+   -v /Users/tiago/Nuxeo/projects/blender-scripts/:/in:ro \
+   -v /Users/tiago/Nuxeo/projects/blender-scripts/output:/out tcardoso/blender \
+   -P /scripts/pipeline.py -- \
+   --input /in/$file_path --outdir /out/$output_dir --operators $operators --lods ${lods[*]// / } --width 960 --height 600 --coords 0,0 90,0 0,90 90,90
 
-  collada2gltf -f $output_dir/conversion-100.dae -o $output_dir/conversion-100.gltf -e
+  docker run --rm -v /Users/tiago/Nuxeo/projects/blender-scripts/output:/in \
+  -v /Users/tiago/Nuxeo/projects/blender-scripts/output:/out tcardoso/collada2gltf -f /in/$output_dir/conversion-100.dae -o /out/$output_dir/conversion-100.gltf -e
 
   for lod in "${lods[@]}"
   do
-    collada2gltf -f $output_dir/conversion-$lod.dae -o $output_dir/conversion-$lod.gltf -e
+    docker run --rm -v /Users/tiago/Nuxeo/projects/blender-scripts/output:/in \
+    -v /Users/tiago/Nuxeo/projects/blender-scripts/output:/out tcardoso/collada2gltf -f /in/$output_dir/conversion-$lod.dae -o /out/$output_dir/conversion-$lod.gltf -e
   done
 
 done
